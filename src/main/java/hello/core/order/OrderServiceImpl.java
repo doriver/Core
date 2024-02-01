463b45e64a3774e6fa7cd36f7d1c354187d01819
doriver <tkdduq241@naver.com>
2024-02-01 14:48:09 +0900

    객체를 생성하고 연결하는 역할과( AppConfig ) 실행하는 역할의( ServiceImpl ) 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,10 +9,19 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private DiscountPolicy discountPolicy; // 이렇게하면 discountPolicy 는 null 값이기 때문에, 아래 코드에서 NullPointerException 발생
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private DiscountPolicy discountPolicy; // 이렇게하면 discountPolicy 는 null 값이기 때문에, 아래 코드에서 NullPointerException 발생
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
