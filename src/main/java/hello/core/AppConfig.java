b82c082cbf836365b2b901d590ba7813159fe5af
doriver <tkdduq241@naver.com>
2024-02-22 18:48:26 +0900

    memberService(), memberRepository(), orderService() 위 3개 에서 생성된 MemoryMemberRepository 는 모두 같은 객체 인스턴스로, 공유되어 사용되고 있다
    memberRepository() 가 3번 호출될꺼로 예상되지만 실제로는 1번만 호출된다

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    // @Bean memberService --> new MemoryMemberRepository()
+    // @Bean orderService --> new MemoryMemberRepository()
+
+    // memberRepository 가 3번 호출될꺼로 예상되지만 실제로는 1번만 호출된다
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
          return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
