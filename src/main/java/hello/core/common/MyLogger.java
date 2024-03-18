d553bf6a206506850a3cd06e5d815c742d790e0b
doriver <tkdduq241@naver.com>
2024-03-18 18:18:03 +0900

    logback.xml 없애니까 로깅쪽 보기좋게 출력,

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Scope(value = "request") // HTTP 요청 당 빈이 하나씩 생성되고, HTTP 요청이 끝나는 시점에 소멸
+public class MyLogger { // 로그출력위한 클래스
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + " [" + requestURL + "] " + message);
+    }
+
+    @PostConstruct // 빈이 생성되는 시점에 자동으로 @PostConstruct 초기화 메서드에 의해, uuid를 생성해서 저장
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close: " + this);
+    }
+
+}
---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 만들어줌 > 생성자가 하나면 자동으로 의존관계 주입 됨
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "Ok";
+    }
+
+}
